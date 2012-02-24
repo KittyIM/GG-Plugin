81bd62b0040010ff514a0f7b73872325e4008e46
arturo182 <arturo182@tlen.pl>
2012-02-24 17:17:37 +0100

    Polish translation added.

---
+++ translations/ggproto_pl.ts
@@ -0,0 +1,256 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>GG::EditDialog</name>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="148"/>
+        <source>Servers</source>
+        <translation>Serwery</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="149"/>
+        <source>If connection to HUB fails, we will try to connect to one of these servers.</source>
+        <translation>Jeżeli połącznie z HUBem się nie powiedzie, będziemy próbowali łączyć się z tymi serwerami.</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="150"/>
+        <source>UIN:</source>
+        <translation>Numer GG:</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="151"/>
+        <source>Password:</source>
+        <translation>Hasło:</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="152"/>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="153"/>
+        <source>Use secure connection</source>
+        <translation>Używaj bezpiecznego połączenia</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="156"/>
+        <source>Previous</source>
+        <translation>Poprzedni</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="157"/>
+        <source>Available</source>
+        <translation>Dostępny</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="158"/>
+        <source>Be right back</source>
+        <translation>Zaraz wracam</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="161"/>
+        <source>Invisible</source>
+        <translation>Niewidoczny</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="163"/>
+        <source>Startup status:</source>
+        <translation>Status początkowy:</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="164"/>
+        <source>Status only visible to friends</source>
+        <translation>Status tylko dla znajomych</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.cpp" line="56"/>
+        <source>Add account</source>
+        <translation>Dodaj konto</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.cpp" line="59"/>
+        <source>Edit account</source>
+        <translation>Edytuj konto</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.cpp" line="99"/>
+        <location filename="../GGEditDialog.cpp" line="104"/>
+        <source>Missing info</source>
+        <translation>Brakujące dane</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.cpp" line="99"/>
+        <source>You have to enter UID</source>
+        <translation>Wpisz numer GG</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.cpp" line="104"/>
+        <source>You have to enter password</source>
+        <translation>Wpisz hasło</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="159"/>
+        <source>Do not disturb</source>
+        <translation>Zajęty</translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="160"/>
+        <source>Free for chat</source>
+        <translation>Chętny do rozmowy</translation>
+    </message>
+</context>
+<context>
+    <name>GG::Account</name>
+    <message>
+        <location filename="../GGAccount.cpp" line="58"/>
+        <source>Contacts</source>
+        <translation>Kontakty</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="59"/>
+        <source>Import</source>
+        <translation>Importuj</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="61"/>
+        <source>From server</source>
+        <translation>Z serwera</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="62"/>
+        <source>From file</source>
+        <translation>Z pliku</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="68"/>
+        <source>Description...</source>
+        <translation>Opis...</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="73"/>
+        <source>Available</source>
+        <translation>Dostępny</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="78"/>
+        <source>Be right back</source>
+        <translation>Zaraz wracam</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="83"/>
+        <source>Free for chat</source>
+        <translation>Chętny do rozmowy</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="88"/>
+        <source>Do not disturb</source>
+        <translation>Zajęty</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="93"/>
+        <source>Invisible</source>
+        <translation>Niewidoczny</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="98"/>
+        <source>Unavailable</source>
+        <translation>Niedostępny</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="275"/>
+        <source>Not connected!</source>
+        <translation>Nie połączony!</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="319"/>
+        <source>New description:</source>
+        <translation>Nowy opis:</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="353"/>
+        <source>Choose file</source>
+        <translation>Wybierz plik</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="353"/>
+        <source>XML files</source>
+        <translation>Pliki XML</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="353"/>
+        <source>Text files</source>
+        <translation>Pliki tekstowe</translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="587"/>
+        <source>Message from</source>
+        <translation>Wiadomość od</translation>
+    </message>
+</context>
+<context>
+    <name>GG::Contact</name>
+    <message>
+        <location filename="../GGContact.cpp" line="50"/>
+        <source>changed description</source>
+        <translation>zmienił opis</translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="63"/>
+        <source>is online</source>
+        <translation>jest dostępny</translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="67"/>
+        <source>is away</source>
+        <translation>zaraz wraca</translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="71"/>
+        <source>is free for chat</source>
+        <translation>jest chętny do rozmowy</translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="75"/>
+        <source>shouldn&apos;t be disturbed</source>
+        <translation>jest zajęty</translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="79"/>
+        <source>is offline</source>
+        <translation>jest niedostępny</translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="83"/>
+        <source>is wtf</source>
+        <translation>jest wtf</translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="119"/>
+        <source>Update avatar</source>
+        <translation>Aktualizuj awatar</translation>
+    </message>
+</context>
+<context>
+    <name>GG::Protocol</name>
+    <message>
+        <location filename="../GGProtocol.cpp" line="19"/>
+        <source>Gadu-Gadu Protocol</source>
+        <translation>Protokół Gadu-Gadu</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/ggproto_untranslated.ts
@@ -0,0 +1,256 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>GG::EditDialog</name>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="148"/>
+        <source>Servers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="149"/>
+        <source>If connection to HUB fails, we will try to connect to one of these servers.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="150"/>
+        <source>UIN:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="151"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="152"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="153"/>
+        <source>Use secure connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="156"/>
+        <source>Previous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="157"/>
+        <source>Available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="158"/>
+        <source>Be right back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="161"/>
+        <source>Invisible</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="163"/>
+        <source>Startup status:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="164"/>
+        <source>Status only visible to friends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.cpp" line="56"/>
+        <source>Add account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.cpp" line="59"/>
+        <source>Edit account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.cpp" line="99"/>
+        <location filename="../GGEditDialog.cpp" line="104"/>
+        <source>Missing info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.cpp" line="99"/>
+        <source>You have to enter UID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.cpp" line="104"/>
+        <source>You have to enter password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="159"/>
+        <source>Do not disturb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGEditDialog.ui"/>
+        <location filename="../ui_GGEditDialog.h" line="160"/>
+        <source>Free for chat</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GG::Account</name>
+    <message>
+        <location filename="../GGAccount.cpp" line="58"/>
+        <source>Contacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="59"/>
+        <source>Import</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="61"/>
+        <source>From server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="62"/>
+        <source>From file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="68"/>
+        <source>Description...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="73"/>
+        <source>Available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="78"/>
+        <source>Be right back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="83"/>
+        <source>Free for chat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="88"/>
+        <source>Do not disturb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="93"/>
+        <source>Invisible</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="98"/>
+        <source>Unavailable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="275"/>
+        <source>Not connected!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="319"/>
+        <source>New description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="353"/>
+        <source>Choose file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="353"/>
+        <source>XML files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="353"/>
+        <source>Text files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGAccount.cpp" line="587"/>
+        <source>Message from</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GG::Contact</name>
+    <message>
+        <location filename="../GGContact.cpp" line="50"/>
+        <source>changed description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="63"/>
+        <source>is online</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="67"/>
+        <source>is away</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="71"/>
+        <source>is free for chat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="75"/>
+        <source>shouldn&apos;t be disturbed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="79"/>
+        <source>is offline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="83"/>
+        <source>is wtf</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GGContact.cpp" line="119"/>
+        <source>Update avatar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GG::Protocol</name>
+    <message>
+        <location filename="../GGProtocol.cpp" line="19"/>
+        <source>Gadu-Gadu Protocol</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
